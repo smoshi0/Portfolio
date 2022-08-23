c91e5bd64ff7c3138a4fa044de8e1be71614a55f
smoshi0 <dirgabayu861@gmail.com>
2022-08-23 20:46:39 +0700

    commit pertama

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navbars = document.querySelectorAll(".nav-link");
+
+// navbars.forEach((n) => {
+//   n.addEventListener("click", function (m) {
+//     navbars.forEach((active) => {
+//       active.classList.remove("border", "rounded");
+//       active.style.backgroundColor = "";
+//     });
+//     m.target.classList.add("border", "rounded");
+//     m.target.style.backgroundColor = "deepskyblue";
+//   });
+// });
